f10124a47752eba367c10f043041ec9c2ccba774
EHRHARDT, Adrien <adrien.ehrhardt@credit-agricole-sa.fr>
2023-06-14 15:16:38 +0000

    [ci-skip] Bumping to version 2.1.16

--- geocoder/__init__.py
+++ geocoder/__init__.py
@@ -11,7 +11,7 @@ geocoder modules and API
 from loguru import logger
 from geocoder.geocoding import search, query
 
-__version__ = "2.1.15"
+__version__ = "2.1.16"
 
 find = search.position
 near = search.reverse
